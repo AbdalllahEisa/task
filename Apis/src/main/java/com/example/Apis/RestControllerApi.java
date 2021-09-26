9ef3d12dda4a6cfbc00bc2f83ce73d22a2409938
AbdalllahEisa <89872569+AbdalllahEisa@users.noreply.github.com>
2021-09-26 20:39:38 +0200

    Update RestControllerApi.java

--- Apis/src/main/java/com/example/Apis/RestControllerApi.java
+++ Apis/src/main/java/com/example/Apis/RestControllerApi.java
@@ -1,97 +1,148 @@
 package com.example.Apis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestControllerApi {
+    private body getBodyJson(String jsonBodyDTO) throws IOException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return new ObjectMapper()
+                .setDateFormat(simpleDateFormat)
+                .readValue(jsonBodyDTO, body.class);
+    }
 
+    @RequestMapping("/ticks")
+    @Cacheable(value="ticks")
+    public Object getBodyDto()
+            throws IOException {
+       String BodyDto= "{\n" +
+               "\n" +
+               "\n" +
+               "  \"instrument\": \"EURUSD\",\n" +
+               "  \"price\": 2.333,\n" +
+               "  \"timestamp\": 1478192204000\n" +
+               "\n" +
+               "\n" +
+               "},{\n" +
+               "\n" +
+               "\n" +
+               "  \"instrument\": \"EURUSD\",\n" +
+               "  \"price\": 2.333,\n" +
+               "  \"timestamp\": 1478192204000\n" +
+               "\n" +
+               "\n" +
+               "}";
 
-    @GetMapping("/ticks")
-    @ResponseBody
-    public Double Prices() throws IOException {
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
         body[] b = null;
         List<Double> y = new ArrayList<>();
+        body i = new body(getBodyJson(BodyDto).getTimestamp(),getBodyJson(BodyDto).getPrice(),getBodyJson(BodyDto).getInstrument());
+
         double r = 0;
-        if (sec >= 1) {
-            ObjectMapper mapper = new ObjectMapper();
+        if (sec >= 0) {
 
-            b = mapper.readValue(new File("C:\\Users\\User\\Desktop\\Apis\\Apis\\src\\main\\resources\\static\\prices.json"), body[].class);
 
-            for (body g : b) {
-                y.add(g.getPrice());
-            }
+
+
+
+
+            return i.getPrice();
+        }else{
+
+
+            return new ResponseEntity<body>(HttpStatus.UNPROCESSABLE_ENTITY);
+
+
         }
-        return y.get(0);
 
     }
 
+
     @GetMapping("/statistics")
+
     @ResponseBody
+    @Cacheable(value="statistics")
     public ResponseEntity<stastics> stasttics() throws IOException {
+        String BodyDto= "{\n" +
+                "\n" +
+                "\n" +
+                "  \"instrument\": \"EURUSD\",\n" +
+                "  \"price\": 2.333,\n" +
+                "  \"timestamp\": 1478192204000\n" +
+                "\n" +
+                "\n" +
+                "},{\n" +
+                "\n" +
+                "\n" +
+                "  \"instrument\": \"EURUSD\",\n" +
+                "  \"price\": 2.333,\n" +
+                "  \"timestamp\": 1478192204000\n" +
+                "\n" +
+                "\n" +
+                "}";
+
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
-        body[] b = null;
-        List<Double> y = new ArrayList<>();
+
+        List<body> y = new ArrayList<>();
+        y.add(getBodyJson(BodyDto));
+        body i = new body(getBodyJson(BodyDto).getTimestamp(),getBodyJson(BodyDto).getPrice(),getBodyJson(BodyDto).getInstrument());
+
         double r = 0;
-        if (sec >= 0) {
-            ObjectMapper mapper = new ObjectMapper();
-
-            b = mapper.readValue(new File("C:\\Users\\User\\Desktop\\Apis\\Apis\\src\\main\\resources\\static\\prices.json"), body[].class);
-
-            for (body g : b) {
-                y.add(g.getPrice());
-            }
-        }
         int count = 0;
-    double avg=0;
+        double avg = 0;
         double u = 0;
         double max = 0;
-        double min=0;
-for(double h:y){
+        double min = 0;
+        if (sec >= 0) {
 
-    count++;
-    u+=h;
 
-}
-        avg=u/count;
 
-        for (int j =0; j < y.size(); j++) {
-            if (y.get(0) >y.get((j-1)+1)) {
-                max = y.get(0);
+                y.add(getBodyJson(BodyDto));
 
-            }else{
-              max=  y.get((j-1)+1);
+            for (body h : y) {
+
+                count++;
+                u += i.getPrice();
 
             }
-        }
+            avg = u / count;
 
+            for (int j = 0; j < y.size(); j++) {
+                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
+                    max = y.get(0).getPrice();
 
-        for (int j =  0; j < y.size(); j++) {
-                if (y.get(0) >y.get((j-1)+1)) {
-                    min = y.get((j-1)+1);
-
-                }else{
-
-                  min=  y.get(0);
+                } else {
+                    max = y.get((j - 1) + 1).getPrice();
 
                 }
             }
 
 
+            for (int j = 0; j < y.size(); j++) {
+                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
+                    min = y.get((j - 1) + 1).getPrice();
+
+                } else {
+
+                    min = y.get(0).getPrice();
+
+                }
+            }
+
+        }
 
         return new ResponseEntity<stastics>(new stastics(count,avg,max,min), HttpStatus.OK);
 
@@ -100,62 +151,75 @@ for(double h:y){
     @GetMapping(" /statistics/{instrument_identifier}")
     @ResponseBody
     public ResponseEntity<stastics> stastticsidentifier(@PathVariable("instrument_identifier")String instrument) throws IOException {
+        String BodyDto = "{\n" +
+                "\n" +
+                "\n" +
+                "  \"instrument\": \"EURUSD\",\n" +
+                "  \"price\": 2.333,\n" +
+                "  \"timestamp\": 1478192204000\n" +
+                "\n" +
+                "\n" +
+                "},{\n" +
+                "\n" +
+                "\n" +
+                "  \"instrument\": \"EURUSD\",\n" +
+                "  \"price\": 2.333,\n" +
+                "  \"timestamp\": 1478192204000\n" +
+                "\n" +
+                "\n" +
+                "}";
+
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
         body[] b = null;
-        List<Double> y = new ArrayList<>();
+        List<body> y = new ArrayList<>();
+body i = new body(getBodyJson(BodyDto).getTimestamp(),getBodyJson(BodyDto).getPrice(),getBodyJson(BodyDto).getInstrument());
+        y.add(getBodyJson(BodyDto));
         double r = 0;
-        if (sec >= 0&&instrument!=null) {
-            ObjectMapper mapper = new ObjectMapper();
-
-            b = mapper.readValue(new File("C:\\Users\\User\\Desktop\\Apis\\Apis\\src\\main\\resources\\static\\prices.json"), body[].class);
-
-            for (body g : b) {
-                y.add(g.getPrice());
-            }
-        }
         int count = 0;
-        double avg=0;
+        double avg = 0;
         double u = 0;
         double max = 0;
-        double min=0;
-        for(double h:y){
+        double min = 0;
+        if (sec >= 0) {
 
-            count++;
-            u+=h;
 
-        }
-        avg=u/count;
+            for (body h : y) {
 
-        for (int j =0; j < y.size(); j++) {
-            if (y.get(0) >y.get((j-1)+1)) {
-                max = y.get(0);
-
-            }else{
-                max=  y.get((j-1)+1);
+                count++;
+                u += i.getPrice();
 
             }
-        }
+            avg = u / count;
 
+            for (int j = 0; j < y.size(); j++) {
+                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
+                    max = y.get(0).getPrice();
 
-        for (int j =  0; j < y.size(); j++) {
-            if (y.get(0) >y.get((j-1)+1)) {
-                min = y.get((j-1)+1);
+                } else {
+                    max = y.get((j - 1) + 1).getPrice();
 
-            }else{
-
-                min=  y.get(0);
-
+                }
             }
+
+
+            for (int j = 0; j < y.size(); j++) {
+                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
+                    min = y.get((j - 1) + 1).getPrice();
+
+                } else {
+
+                    min = y.get(0).getPrice();
+
+                }
+            }
+
         }
+            return new ResponseEntity<stastics>(new stastics(count, avg, max, min), HttpStatus.OK);
 
-
-
-        return new ResponseEntity<stastics>(new stastics(count,avg,max,min), HttpStatus.OK);
 
 
     }
-
 
 }
 
